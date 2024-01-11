59aa4ebab8bf754e1e11f8371937c466ee168fb0
Mac <mac@MacBook-Pro-Mac.local>
2024-01-11 15:45:32 +0300

    add validation for form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from '../auth.service';
 import { ApiResponseItem } from '../response.types';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -12,41 +13,50 @@ import { ApiResponseItem } from '../response.types';
 export class LoginComponent implements OnInit {
 
   private readonly LOGIN_API_URL: string = 'http://51.158.107.27:82/api/login';
+  showPassword: boolean = false;
+  form: FormGroup;
 
   constructor(
     private router: Router,
     private http: HttpClient,
     private authService: AuthService,
-  ) {}
+    private formBuilder: FormBuilder
+  ) {
+    this.form = this.formBuilder.group({
+      username: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required, Validators.minLength(5)]],
+      rememberMe: [false]
+    })
+  }
 
-  onSubmit(form: any): void {
-    const username = form.value.username;
-    const password = form.value.password;
-
-    if (username === 'morzac1@gmail.com' && password === 'sT6ohz') {
+  onSubmit(): void {
+    if (this.form.valid) {
       const loginData = {
-        login: username,
-        password: password
+        login: this.form.value.username,
+        password: this.form.value.password
       };
-
       this.http.post<ApiResponseItem>(this.LOGIN_API_URL, loginData)
         .subscribe(response => {
            if (!response.hasError) {
             if (response.userInfo && response.tokens) {
               this.authService.setTokens(response.tokens.token, response.tokens.refreshToken);
-
               this.authService.setLoginResponse(response);
+              this.authService.isAuthenticated();
               this.router.navigate(['/dashboard']);
-                
             } else {
-              // alert('sdfsdfsdfs');
+              alert('sdfsdfsdfs');
           }
-        }});
-    } else {
-       alert('sdfsdfsdfs');
+      }});
     }
-  } 
+  }
+
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+    const passwordInput = document.getElementById('password') as HTMLInputElement;
+    passwordInput.type = this.showPassword ? 'text' : 'password';
+  }
 
   ngOnInit(): void {
   }
+
 }
